3a7db7321f9b7031cfdd0e288ad926fdba7a96de
azd19 <azd19@mails.tsinghua.edu.cn>
2022-08-13 22:31:33 +0800

    cmt

--- samples/rust/rust_kvm/rust_kvm.rs
+++ samples/rust/rust_kvm/rust_kvm.rs
@@ -2,11 +2,13 @@
 
 //! Rust KVM for VMX
 #[allow(missing_docs)]
+use core::cfg;
+use core::arch::asm;
 use kernel::c_types::c_void;
 use kernel::mm::virt::Area;
 use kernel::prelude::*;
 use kernel::{
-    bindings, bit,
+    bindings, bit, Error,
     file::File,
     file_operations::{FileOperations, IoctlCommand, IoctlHandler},
     io_buffer::{IoBufferReader, IoBufferWriter},
@@ -130,6 +132,11 @@ struct RustMiscdev {
 
 impl KernelModule for RustMiscdev {
     fn init(name: &'static CStr, module: &'static ThisModule) -> Result<Self> {
+        if cfg!(CONFIG_X86_32) {
+            pr_err!("RustKvm doesn't support 32-bit machine\n");
+            return Err(Error::ENOTSUPP);
+        }
+        
         pr_info!("Rust kvm module (init) name={:?} \n", name);
         {
             let _lock = module.kernel_param_lock();
@@ -153,6 +160,20 @@ impl Drop for RustMiscdev {
 }
 
 static mut VMXON_VMCS: Option<RkvmPage> = None;
+fn rkvm_vmxon(addr: u64) {
+    let mut cr4: u64 = read_cr4();
+    cr4 |= Cr4::CR4_ENABLE_VMX as u64;
+    write_cr4(cr4);
+    rkvm_debug!("written new cr4 value\n");
+    unsafe {
+        asm!(
+            "vmxon ({0})",
+            in(reg) &addr,
+            options(att_syntax)
+        );
+    }
+}
+
 fn rkvm_set_vmxon(state: &RkvmState) -> Result<u32> {
     let revision_id = state.inner.lock().vmcsconf.revision_id;
     let vmcs = alloc_vmcs(revision_id);
@@ -161,7 +182,7 @@ fn rkvm_set_vmxon(state: &RkvmState) -> Result<u32> {
         Err(err) => return Err(/*Error::ENOMEM*/ err),
     };
 
-    let vmxe = unsafe { bindings::native2_read_cr4() & bit(x86reg::Cr4::CR4_ENABLE_VMX) };
+    let vmxe = read_cr4() & Cr4::CR4_ENABLE_VMX as u64;
 
     rkvm_debug!("Rust kvm :vmxe {:}\n", vmxe);
 
@@ -174,7 +195,7 @@ fn rkvm_set_vmxon(state: &RkvmState) -> Result<u32> {
 
         rkvm_debug!(" pa = {:x}\n", pa);
 
-        bindings::rkvm_vmxon(pa);
+        rkvm_vmxon(pa);
         VMXON_VMCS = Some(vmcs);
     }
     Ok(0)
--- samples/rust/rust_kvm/vmcs.rs
+++ samples/rust/rust_kvm/vmcs.rs
@@ -322,7 +322,7 @@ pub(crate) struct VmcsConfig {
 }
 
 fn vmcs_status() -> Result {
-    let rflags = unsafe { bindings::rkvm_rflags_read() };
+    let rflags = read_rflags();
     if rflags & (RFlags::FLAGS_ZF as u64 + RFlags::FLAGS_CF as u64) != 0 {
         return Err(Error::EINVAL);
     }
@@ -741,9 +741,9 @@ impl VmcsConfig {
         Ok(0)
     }
     pub(crate) fn set_host_constant_vmcs(&self) {
-        let mut cr0 = unsafe { bindings::native2_read_cr0() };
+        let mut cr0 = read_cr0();
         cr0 &= !(Cr0::CR0_TS as u64);
-        let cr3 = unsafe { bindings::native2_read_cr3() };
+        let cr3 = read_cr3();
         // let cr4 = unsafe { bindings::cr4_read_shadow() };
         vmcs_write64(VmcsField::HOST_CR0, cr0);
         vmcs_write64(VmcsField::HOST_CR3, cr3);
@@ -757,7 +757,7 @@ impl VmcsConfig {
         vmcs_write16(VmcsField::HOST_GS_SELECTOR, 0); /* 22.2.4 */
         vmcs_write16(VmcsField::HOST_TR_SELECTOR, 64);
         // let fs = read_msr(X86Msr::FS_BASE);
-        let fs = unsafe { bindings::rkvm_rdfsbase() };
+        let fs = read_fsbase();
         vmcs_write64(VmcsField::HOST_FS_BASE, fs);
         // let gs = read_msr(X86Msr::GS_BASE);
         let gs = unsafe { bindings::rkvm_rdgsbase() };
